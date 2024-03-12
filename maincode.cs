440edc5de75d5cdc18719a4160a41a0669837c6d
Freddy Nyree <163116788+whiphat@users.noreply.github.com>
2024-03-12 11:15:05 +0200

    Create maincode.cs

---
+++ maincode.cs
@@ -0,0 +1,26 @@
+   public static (int[], int, int) LoadBitmap(string filename)
+    {
+        using var image = Image.Load<Bgra32>(filename);
+        int width = image.Width, height = image.Height;
+        int[] result = new int[width * height];
+        image.CopyPixelDataTo(MemoryMarshal.Cast<int, Bgra32>(result));
+        return (result, width, height);
+    }
+
+    unsafe public static void SaveBitmap(int[] data, int width, int height, string filename)
+    {
+        fixed (int* pData = data)
+        {
+            using var image = Image.WrapMemory<Bgra32>(pData, width, height);
+            image.SaveAsPng(filename);
+        }
+    }
+}  protected double[] weights;
+    double[] weightLogWeights, distribution;
+
+    protected int[] sumsOfOnes;
+    double sumOfWeights, sumOfWeightLogWeights, startingEntropy;
+    protected double[] sumsOfWeights, sumsOfWeightLogWeights, entropies;
+
+    public enum Heuristic { Entropy, MRV, Scanline };
+    Heuristic heuristic;
